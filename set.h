0e7be2cd25799530918ac720bdc01598b10c8d46
arushi-lu <105436080+arushi-lu@users.noreply.github.com>
2022-05-12 22:19:58 +0600

    set (bst)
    bst implementation

--- set.h
+++ set.h
@@ -3,116 +3,100 @@
 #define SET_
 
 #include <iostream>
-#include <vector>
-#include <list>
 #include <string> 
-#include <cmath>
 
-namespace case_insensitive
+// It is good to have a look at the list-based implementation
+// of queue again. The implementation here is very similar.
+
+struct treenode
 {
-   size_t hash( const std::string& s );
-   bool equal( const std::string& s1, const std::string& s2 ); 
-}
+   std::string value; 
+   treenode* left;
+   treenode* right;
 
-
-class set 
-{
-   size_t set_size;         // Total number of elements.
-
-   double max_load_factor;
-
-   using buckettype = std::list< std::string > ; 
-      // The type of a bucket. Don't use typedef in C++ . 
-
-   std::vector< buckettype > buckets;
-
-   bool insert_norehash( const std::string& s );
-      // Returns true if the insertion took place.
-
-public:
-   // Number of buckets cannot be zero. 
-   // Different from earlier interpretations, we use 
-   // powers of three. Best would be prime numbers close
-   // to powers of two. 
-   
-   set( double max_load_factor = 3.0 ) 
-      : set_size(0),
-        max_load_factor( max_load_factor ),
-        buckets( std::vector< buckettype > (8) )
-   { 
-      if( max_load_factor <= 0.0 )   
-         throw std::runtime_error( "max load factor must be > 0" ); 
-   }
-
-   // We define the copy constructor, assignment operator, and destructor
-   // as defaults, which means that they are just applied on the fields.
-
-   set( const set& s ) = default; 
-   set& operator = ( const set& s ) = default; 
-   ~set() = default; 
-
-   set( std::initializer_list<std::string> init, double max_load_factor = 3 ); 
-
-   bool contains( const std::string& s ) const; 
-   bool insert( const std::string& s );
-      // Returns true if the insertion took place.
-
-   bool remove( const std::string& s );
-      // Returns true if s was found and removed.
-
-   size_t size() const;
-
-   bool empty() const;
-
-   void clear();
-      // No need to decrease the number of buckets.
-     
-   void consider_rehash( );
-      // Is this a const operation? Somewhat.
- 
-   void print( std::ostream& out ) const; 
-
-
-   // The functions below are not part of 
-   // the set interface, they are specific for hashing:
-
-   double loadfactor() const 
-   {
-      return static_cast<double> ( set_size ) / buckets. size( ); 
-         // C-style casts of form (double) set_size are deprecated. 
-   }
-
-   double standarddev() const 
-   {
-      double sum = 0.0;
-      double lf = loadfactor();
-
-      for ( const auto& l : buckets ) 
-      {
-	  double dif = l. size( ) - lf;
-          sum += dif*dif;
-      }
-
-      return sqrt( sum / buckets. size( ));
-   }
-  
-   std::ostream& printstatistics( std::ostream& out ) const
-   {
-      out << "set size =            " << size( ) << "\n";
-      out << "load factor =         " << loadfactor( ) <<
-          " ( max = " << max_load_factor << " )\n";
-      out << "standard deviation =  " << standarddev( ) << "\n";
-      return out;
-   }
+   treenode( const std::string& value, 
+             treenode* left = nullptr, treenode* right = nullptr )
+      : value( value ),
+        left( left ),
+        right( right )  
+   { }
 
 };
 
-inline std::ostream& operator << ( std::ostream& out, const set& s ) 
+
+const treenode* find( const treenode* n, const std::string& s );
+   // Returns nullptr if not found.
+
+treenode** find( treenode** n, const std::string& s ); 
+   // Returns a pointer to nullptnr if not found. 
+
+size_t size( const treenode* n ); 
+   // Returns the number of values stored in the tree. 
+
+void deallocate( treenode* n ); 
+   // Deallocate the complete tree below n.
+
+treenode* makecopy( const treenode* n );
+   // Make a copy of the tree below n.
+
+void print( std::ostream& out, const treenode* n, unsigned int indent );
+   // Print the tree as tree.
+
+size_t height( const treenode* n );
+   // Returns the height of the tree starting at n.
+
+unsigned int log_base2( size_t s );
+
+
+class set
+{
+   treenode* tr;
+
+public: 
+   set( ) 
+      : tr( nullptr ) 
+   { } 
+
+   set( const set& other );
+
+   set& operator = ( const set& other );
+
+   ~set( );
+
+   set( std::initializer_list< std::string > init )
+      : tr( nullptr ) 
+   {
+      for( const auto& s: init ) 
+         insert(s);
+   } 
+  
+   bool contains( const std::string& s ) const;
+
+   bool insert( const std::string& s );
+      // Returns true if the insertion took place.
+
+   size_t size( ) const; 
+   size_t height( ) const; 
+
+   bool empty( ) const; 
+      // Must work in constant time!
+
+   void clear( );
+
+   void checksorted( ) const;
+      // Check if the tree is sorted, and throw an exception
+      // if it is not. Call this function often to check your
+      // trees, but make your final performance tests without it.  
+
+   void print( std::ostream& out, unsigned int indent = 0 ) const;
+};
+
+
+inline std::ostream& operator << ( std::ostream& out, const set& s )
 {
    s. print( out );
-   return out;
+   return out; 
 }
 
 #endif
-
 
